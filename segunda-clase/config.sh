71ccfc275a9638f2bed9c87a9982458608bba71b
John <133547897+Johny-zu@users.noreply.github.com>
2025-03-14 16:58:18 -0600

    avance hasta el uso de mysql

--- segunda-clase/config.sh
+++ segunda-clase/config.sh
@@ -9,3 +9,15 @@ sudo apt install mysql-server
 sudo mysql -V
 
 sudo systemctl status mysql
+
+cd /var/backups
+
+# creo que le falta un -p al final
+sudo mysql -u root 
+
+# ya estamos en mysql
+show databases;
+
+# nos movemos a la base de datos de mysql 
+use mysql;
+
